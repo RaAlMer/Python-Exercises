25696aca9ae3f1701cc828378af8b1f3d8bab543
Raúl Alonso <67586098+RaAlMer@users.noreply.github.com>
2024-03-09 01:36:42 +0100

    Update and rename tic tac toe.py to Cisco Course 1/tic tac toe.py

--- Cisco Course 1/tic tac toe.py
+++ Cisco Course 1/tic tac toe.py
@@ -10,8 +10,8 @@ initial_board = [
 ]
 
 def display_board(board):
-    # La función acepta un parámetro el cual contiene el estado actual del tablero
-    # y lo muestra en la consola.    
+    # The function takes a parameter containing the current state of the board
+    # and displays it on the console.    
     print("+-------" * 3,"+", sep="")
     for row in range(3):
         print("|       " * 3,"|", sep="")
@@ -22,16 +22,16 @@ def display_board(board):
         print("+-------" * 3,"+",sep="")
 
 def enter_move(board):
-    # La función acepta el estado actual del tablero y pregunta al usuario acerca de su movimiento,  
-    # verifica la entrada y actualiza el tablero acorde a la decisión del usuario.
+    # The function takes the current state of the board and asks the user about their move,  
+    # checks the input, and updates the board according to the user's decision.
     display_board(board)
-    movement = int(input('Ingresa tu movimiento: '))
+    movement = int(input('Input your movement: '))
     if movement not in range(1, 10):
-        print('Ingresa un numero del 1 al 9')
-        movement = int(input('Ingresa tu movimiento: '))
+        print('Input a number between 1 and 9')
+        movement = int(input('Input your movement: '))
     while divmod(movement - 1, 3) not in make_list_of_free_fields(board):
-        print('Ese cuadro está ocupado')
-        movement = int(input('Elige otro: '))
+        print('That square is not empty')
+        movement = int(input('Choose another one: '))
     if movement <= 3:
         board[0][movement - 1] = PLAYER_O
     elif movement <= 6:
@@ -42,33 +42,33 @@ def enter_move(board):
         board[2][movement] = PLAYER_O
 
 def make_list_of_free_fields(board):
-    # La función examina el tablero y construye una lista de todos los cuadros vacíos. 
-    # La lista esta compuesta por tuplas, cada tupla es un par de números que indican la fila y columna.
+    # The function examines the board and builds a list of all empty squares.
+    # The list is composed of tuples, where each tuple is a pair of numbers indicating the row and column.
     empty_positions = [(row, col) for row in range(3) for col in range(3) if board[row][col] not in [PLAYER_X, PLAYER_O]]
     return empty_positions
 
 def victory_for(board, sign):
-    # La función analiza el estatus del tablero para verificar si 
-    # el jugador que utiliza las 'O's o las 'X's ha ganado el juego.
+    # The function analyzes the status of the board to check 
+    # if the player using 'O's or 'X's has won the game.
     # Assuming board is a 3x3 list and sign is the player's sign
     for row in range(3):
         for col in range(3):
             if col + 2 < 3 and board[row][col] == sign and board[row][col + 1] == sign and board[row][col + 2] == sign:
-                print('¡Has Ganado!: ', sign)
+                print('You won!: ', sign)
                 return True
             if row + 2 < 3 and board[row][col] == sign and board[row + 1][col] == sign and board[row + 2][col] == sign:
-                print('¡Has Ganado!: ', sign)
+                print('You won!: ', sign)
                 return True
             if row == 0 and col == 0 and board[row][col] == sign and board[row + 1][col + 1] == sign and board[row + 2][col + 2] == sign:
-                print('¡Has Ganado!: ', sign)
+                print('You won!: ', sign)
                 return True
             if row == 0 and col == 2 and board[row][col] == sign and board[row + 1][col - 1] == sign and board[row + 2][col - 2] == sign:
-                print('¡Has Ganado!: ', sign)
+                print('You won!: ', sign)
                 return True
     return False
             
 def draw_move(board):
-    # La función dibuja el movimiento de la máquina y actualiza el tablero.
+    # The function draws the machine's move and updates the board.
     movement_machine = randrange(10)
     while divmod(movement_machine - 1, 3) not in make_list_of_free_fields(board):
         movement_machine = randrange(10)
@@ -89,14 +89,14 @@ def main(board):
         if victory_for(board, PLAYER_O):
             break
         elif not make_list_of_free_fields(board):
-            print('¡Empate!')
+            print('Draw!')
             break
         draw_move(board)
         display_board(board)
         if victory_for(board, PLAYER_X):
             break
         elif not make_list_of_free_fields(board):
-            print('¡Empate!')
+            print('Draw!')
             break
     display_board(board)
     
